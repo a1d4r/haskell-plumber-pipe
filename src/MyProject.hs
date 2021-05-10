a0613e3e8b03f33054fbf77c04096d9461508300
Aidar Garikhanov <a1d4r@yandex.ru>
2021-05-10 20:15:53 +0300

    Add level 1

--- src/MyProject.hs
+++ src/MyProject.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax #-}
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fdefer-typed-holes -fshow-hole-constraints -funclutter-valid-hole-fits #-}
 module MyProject where
 
 import CodeWorld
+import Data.List.Split
 
 
 run :: IO ()
@@ -13,10 +15,10 @@ handleGame :: Event -> GameState -> GameState
 handleGame event gameState =
   case gameState of
     StartMenu -> handleMenu event
-    Won pic -> handleEndScreen event 
+    Won pic -> handleEndScreen event
     Lost pic -> handleEndScreen event
-    InGame lvl -> handleLevel (InGame lvl) event
-    Flows flowLvl t -> handleFlows (Flows flowLvl t) event
+    InGame lvl -> handleLevel event (InGame lvl)
+    Flows flowLvl t -> handleFlows event (Flows flowLvl t)
 
 
 handleMenu :: Event -> GameState
@@ -25,19 +27,19 @@ handleMenu = _
 handleEndScreen :: Event -> GameState
 handleEndScreen = _
 
-handleLevel :: Event -> GameState
-handleLevel (InGame lvl) event = _
+handleLevel :: Event -> GameState -> GameState
+handleLevel = _
 
-handleFlows :: Event -> GameState
+handleFlows :: Event -> GameState -> GameState
 handleFlows event = _
 
 drawGame :: GameState -> Picture
-drawGame gameState =  
+drawGame gameState =
   case gameState of
     StartMenu -> drawMenu
-    Won -> drawWonScreen
-    Lost -> drawLostScreen
-    InGame lvl -> drawLevel lvl 
+    Won pic -> drawWonScreen
+    Lost pic -> drawLostScreen
+    InGame lvl -> drawLevel lvl
     Flows flowLevel t -> drawFlowScreen flowLevel t
 
 drawMenu :: Picture
@@ -53,18 +55,13 @@ drawLevel :: Level -> Picture
 drawLevel lvl = _
 
 drawFlowScreen :: FlowLevel -> Double -> Picture
-drawFlowScreen flowLvl t = _ 
+drawFlowScreen flowLvl t = _
 
 -- | Draw one cell at given coordinates
 drawCellAt :: Int -> Int -> Cell -> Picture
-drawCellAt i j cell = translated x y
-  (rectangle 1 1 <> cellPicture)
-  where
-    x = fromIntegral i
-    y = fromIntegral j
-    cellPicture 
+drawCellAt i j cell = _
 
-data GameState 
+data GameState
   = StartMenu              -- ^ Start menu
   | Won Picture            -- ^ Player won (connected pipes correctly)
   | Lost Picture           -- ^ Player lost (water leakage)
@@ -73,23 +70,24 @@ data GameState
 
 -- | Pipe that connects sides specified by True
 -- | sides are specified in order Up, Right, Down, Left
-data Pipe 
-  = ConnectivePipe   -- ^ Pipes connecting several sides
-  {                  -- ^ open sides are marked as True
+data Pipe
+  = ConnectivePipe   -- ^ Pipes connecting several sides, open sides are marked as True
+  {
     up :: Bool,
-    right :: Bool, 
-    left :: Bool, 
-    down :: Bool 
+    right :: Bool,
+    left :: Bool,
+    down :: Bool
   }
   | SourcePipe
   | DestinationPipe
+  deriving (Show)
 
 -- | A cell is either empty or has pipe in it
 -- | Some Cells are meant to be empty by designers of the level
 type Cell = Maybe Pipe
 
 -- | A cell which can be either empty or filled with water 
-type FlowCell = FilledCell Cell | EmptyCell Cell
+data FlowCell = FilledCell Cell | EmptyCell Cell
 
 -- | A level is a 2d grid of cells
 type Level = [[Cell]]
@@ -101,3 +99,46 @@ type FlowLevel = [[FlowCell]]
 emptyLevel :: (Int, Int) -> Level
 emptyLevel (n, m) = replicate n (replicate m Nothing)
 
+
+-- | Convert string representation of level to level type 
+stringsToLevel :: [String] -> Level
+stringsToLevel = map (map strToCell . splitOn " ")
+  where
+    strToCell :: String -> Cell
+    strToCell c =
+      case c of
+        "╼" -> Just SourcePipe
+        "╾" -> Just DestinationPipe
+        "┃" -> Just (ConnectivePipe True False True False)
+        "━" -> Just (ConnectivePipe False True True False)
+        "┏" -> Just (ConnectivePipe False True True False)
+        "┓" -> Just (ConnectivePipe False False True True)
+        "┗" -> Just (ConnectivePipe True True False False)
+        "┛" -> Just (ConnectivePipe True False False True)
+        "┣" -> Just (ConnectivePipe True True True False)
+        "┫" -> Just (ConnectivePipe True False True True)
+        "┳" -> Just (ConnectivePipe False True True True)
+        "┻" -> Just (ConnectivePipe True True False True)
+        "╋" -> Just (ConnectivePipe True True True True)
+        _ -> Nothing
+
+
+-- | Symbol representation for level
+-- | Character set for pipes: https://unicode-table.com/en/blocks/box-drawing/
+reprOfLevel1 :: [String]
+reprOfLevel1 =
+  [ "╼ ┓ ┏ ┳ ━ ┓ ┏ ━ ━ ┓"
+  , ". ┃ ┃ ┃ . ┃ ┗ ┓ . ┃"
+  , ". ┃ ┃ ┃ . ┃ . ┃ ┏ ┛"
+  , ". ┗ ┛ ┃ . ┣ ━ ┛ ┃ ."
+  , "┏ ┓ . ┃ . ┃ ┏ ━ ┛ ."
+  , "┃ ┗ ━ ┻ ━ ┛ ┃ . ┏ ┓"
+  , "┗ ┳ ━ ┳ ━ ━ ╋ ━ ┛ ┃"
+  , ". ┃ . ┃ ┏ ━ ┻ ━ ━ ┛"
+  , ". ┃ ┏ ┛ ┃ . . . . ."
+  , ". ┗ ┛ . ┗ ━ ━ ━ ━ ╾"
+  ]
+
+
+level1 :: Level
+level1 = stringsToLevel reprOfLevel1
