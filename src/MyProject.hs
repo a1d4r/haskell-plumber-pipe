8f0ca55d4d6ae097403ea0b18db8d581725f03ad
TooTiredOne <kamaliyevkamil@gmail.com>
2021-05-11 19:28:06 +0300

    + drawing cell with pipe

--- src/MyProject.hs
+++ src/MyProject.hs
@@ -4,17 +4,18 @@
 module MyProject where
 
 import CodeWorld
+import Data.Text as Text
 
 
 run :: IO ()
-run = activityOf StartMenu handleGame drawGame
-
+-- run = activityOf StartMenu handleGame drawGame
+run = drawingOf (drawCellAt 1 1 (Just (ConnectivePipe True True True True)))
 handleGame :: Event -> GameState -> GameState
 handleGame event gameState =
   case gameState of
     StartMenu -> handleMenu event
-    Won pic -> handleEndScreen event 
-    Lost pic -> handleEndScreen event
+    Won pic   -> handleEndScreen event 
+    Lost pic  -> handleEndScreen event
     InGame lvl -> handleLevel (InGame lvl) event
     Flows flowLvl t -> handleFlows (Flows flowLvl t) event
 
@@ -25,18 +26,18 @@ handleMenu = _
 handleEndScreen :: Event -> GameState
 handleEndScreen = _
 
-handleLevel :: Event -> GameState
+handleLevel :: GameState -> Event -> GameState
 handleLevel (InGame lvl) event = _
 
-handleFlows :: Event -> GameState
+handleFlows :: GameState -> Event -> GameState
 handleFlows event = _
 
 drawGame :: GameState -> Picture
 drawGame gameState =  
   case gameState of
     StartMenu -> drawMenu
-    Won -> drawWonScreen
-    Lost -> drawLostScreen
+    Won pic -> drawWonScreen
+    Lost pic -> drawLostScreen
     InGame lvl -> drawLevel lvl 
     Flows flowLevel t -> drawFlowScreen flowLevel t
 
@@ -55,6 +56,37 @@ drawLevel lvl = _
 drawFlowScreen :: FlowLevel -> Double -> Picture
 drawFlowScreen flowLvl t = _ 
 
+-- | Draw a signle pipe
+drawPipe :: Pipe -> Picture
+drawPipe SourcePipe = lettering (Text.pack "\x2707")
+drawPipe DestinationPipe = lettering (Text.pack "\x1F6C0")
+drawPipe (ConnectivePipe False True True False) -- "━"
+   = solidRectangle 1 0.2  
+drawPipe (ConnectivePipe True False False True) -- "┃"
+  = solidRectangle 0.2 1  
+drawPipe (ConnectivePipe True False True False)  -- "┛"
+  = solidPolygon [(-0.5, -0.1), (0.1, -0.1), (0.1, 0.5), (-0.1, 0.5), (-0.1, 0.1), (-0.5, 0.1)]
+drawPipe (ConnectivePipe False False True True) -- "┓"
+  = rotated (pi/2) (drawPipe (ConnectivePipe True False True False))
+drawPipe (ConnectivePipe False True False True) -- "┏"
+  = rotated (pi) (drawPipe (ConnectivePipe True False True False))
+drawPipe (ConnectivePipe True True False False) -- "┗"
+  = rotated (-pi/2) (drawPipe (ConnectivePipe True False True False))
+drawPipe (ConnectivePipe True True True False) -- "┻"
+  = solidPolygon [(-0.5, -0.1), (0.5, -0.1), (0.5, 0.1), 
+                  (0.1, 0.1), (0.1, 0.5), (-0.1, 0.5), 
+                  (-0.1, 0.1), (-0.5, 0.1)]
+drawPipe (ConnectivePipe True False True True) -- "┫"
+  = rotated (pi/2) (drawPipe (ConnectivePipe True True True False))
+drawPipe (ConnectivePipe True True False True) -- "┣"
+  = rotated (-pi/2) (drawPipe (ConnectivePipe True True True False))
+drawPipe (ConnectivePipe False True True True) -- "┳"
+  = rotated (pi) (drawPipe (ConnectivePipe True True True False))
+drawPipe (ConnectivePipe True True True True) -- "╋"
+  = drawPipe (ConnectivePipe True False True True)  
+    <> drawPipe (ConnectivePipe True True False True)
+
+
 -- | Draw one cell at given coordinates
 drawCellAt :: Int -> Int -> Cell -> Picture
 drawCellAt i j cell = translated x y
@@ -62,7 +94,10 @@ drawCellAt i j cell = translated x y
   where
     x = fromIntegral i
     y = fromIntegral j
-    cellPicture 
+    cellPicture =
+      case cell of 
+        Nothing -> blank
+        Just p  -> drawPipe p
 
 data GameState 
   = StartMenu              -- ^ Start menu
@@ -89,7 +124,7 @@ data Pipe
 type Cell = Maybe Pipe
 
 -- | A cell which can be either empty or filled with water 
-type FlowCell = FilledCell Cell | EmptyCell Cell
+data FlowCell = FilledCell Cell | EmptyCell Cell
 
 -- | A level is a 2d grid of cells
 type Level = [[Cell]]
@@ -99,5 +134,5 @@ type FlowLevel = [[FlowCell]]
 
 -- | Empty level of size n x m
 emptyLevel :: (Int, Int) -> Level
-emptyLevel (n, m) = replicate n (replicate m Nothing)
+emptyLevel (n, m) = Prelude.replicate n (Prelude.replicate m Nothing)
 
